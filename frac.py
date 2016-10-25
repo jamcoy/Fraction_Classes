52023b5f0df2f56c18d7f307db1d1d07285a1dd4
jamcoy <james@coynemail.net>
2016-10-25 20:31:18 +0100

    Handle vulgar fractions

--- frac.py
+++ frac.py
@@ -7,10 +7,20 @@ class Fraction(object):
         self.den = denominator
 
     def __repr__(self):
-        divisor = gcd(self.num, self.den)
-        smallest_num = self.num / divisor
-        smallest_den = self.den / divisor
-        return '%s/%s' % (smallest_num, smallest_den)
+        if self.num >= self.den:
+            qd = divmod(self.num, self.den)
+            whole_number = qd[0]
+            remaining_num = qd[1]
+            remaining_den = self.den
+            if qd[1] == 0:
+                return 'Vulgar fraction equates to: %s' % whole_number
+            else:
+                return 'Vulgar fraction equates to: %s %s/%s' % (whole_number, remaining_num, remaining_den)
+        else:
+            divisor = gcd(self.num, self.den)
+            smallest_num = self.num / divisor
+            smallest_den = self.den / divisor
+            return '%s/%s' % (smallest_num, smallest_den)
 
     def __add__(self, other):
         num = self.num * other.den + self.den * other.num
@@ -29,10 +39,10 @@ class Fraction(object):
         return Fraction(self.num * other.den, self.den * other.num)
 
 
-f1 = Fraction(1, 2)
+f1 = Fraction(3, 2)
 f2 = Fraction(1, 4)
-print "f1, f2"
-print f1, f2
+print "f1 = 3/2"
+print "f2 = 1/4"
 print
 
 print "f1 + f2"
